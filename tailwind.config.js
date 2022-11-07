3856e72f2b89bd1ec7a39d72eda53c08a2e82335
dastin <astindillon@gmail.com>
2022-11-07 14:05:20 -0500

    init and setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // eslint-disable-next-line prettier/prettier
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
